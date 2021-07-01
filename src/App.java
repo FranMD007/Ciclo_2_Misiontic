ee781017661fd07c2088c05766d16bf12cbe44a4
FranMD007 <fran.2305@utp.edu.co>
2021-07-01 18:11:13 -0500

    Verificar si se borró ua línea

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         int variable=8;
         System.out.println(variable);
         boolean menos=false;
